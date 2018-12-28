a286b1000df45ff52f8933aa4e23cc1eb9e7db5d
Jacek Malec <jac3km4@gmail.com>
2018-12-28 23:20:08 +0000

    Improve the API

--- test/examples/Stuff.purs
+++ test/examples/Stuff.purs
@@ -1,33 +1,40 @@
 module Stuff where
-import Spaz (ActionHandler, Element, wiredEq, defaultSpec, dispatch, modify, state, zoom)
-import Spaz.DOM (div, div', text)
-import Spaz.Props (onClick)
 import Control.Applicative ((*>))
 import Data.Lens (Lens', lens)
 import Effect (Effect)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Prelude (Unit, const, show, ($), (+))
-import Spaz.HTML(mount)
+import Prelude (Unit, const, show, ($), (+), (<<<))
+import Spaz (ActionHandler, Component, Element, defaultSpec, dispatch, element, foreachZ, modify, stateless, wiredEq, zoom)
+import Spaz.DOM (div, div', text)
+import Spaz.HTML (mount)
+import Spaz.Props (onClick)
 
-type State = { number :: Int }
+type State = { number :: Int, items :: Array Int }
 
 data Action = ResetCounter
 
 _number :: ∀ r. Lens' { number :: Int | r } Int
 _number = lens (_.number) (_ { number = _ })
 
-page :: Element State Action
-page = div' [div [onClick $ \_ -> dispatch ResetCounter] [text "hehe"], zoom _number stuff]
+_items :: ∀ r. Lens' { items :: (Array Int) | r } (Array Int)
+_items = lens (_.items) (_ { items = _ })
 
-stuff :: ∀ act. Element Int act
-stuff = state $ \st ->
+page :: Element State Action
+page = div' [ div [onClick $ \_ -> dispatch ResetCounter] [text "hehe"]
+            , zoom _number $ element stuff
+            , foreachZ _items $ element $ stateless $ text <<< show
+            ]
+
+stuff :: ∀ act. Component Int act
+stuff = wiredEq defaultSpec $ \st ->
   div 
     [onClick $ \_ -> liftEffect (log "clicked") *> modify \s -> s + 1]
     [text $ show st]
 
 main :: Effect Unit
-main = mount "main" (wiredEq defaultSpec page) handler {number:0}
+main = mount "main" page handler {number:0, items: [2]}
 
 handler :: ActionHandler State Action
-handler ResetCounter = modify (const {number: 0})
+handler ResetCounter = modify (const {number: 0, items: []})
+
